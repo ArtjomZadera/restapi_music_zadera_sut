7ef3983d3d2305f50763916caf78361f9cb80b1d
Satana500 <artjom.zadera@ivkhk.ee>
2024-05-31 11:35:34 +0300

    update author routes

---
+++ routes/authorRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authorController = require('../controllers/authorController');
+const authJwt = require('../middleware/authJwt');
+
+// Define routes for authors
+router.get('/authors', authorController.showAuthorList);
+router.get('/authors/:id', authorController.findAuthorById);
+router.get('/authors/search/:name', authorController.findAuthorByName);
+
+router.post('/authors', authJwt.verifyToken, authJwt.checkUserId, authorController.createAuthor);
+router.put('/authors/:id', authJwt.verifyToken, authJwt.checkUserId, authorController.updateAuthor);
+router.delete('/authors/:id', authJwt.verifyToken, authJwt.checkUserId, authorController.deleteAuthor);
+
+module.exports = router;
