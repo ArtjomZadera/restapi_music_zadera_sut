d89043ba58cdd892f2c14dae51da9d129c704363
AlekseiSut <113894886+AlekseiSut@users.noreply.github.com>
2024-06-03 13:31:09 +0300

    New Share

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.use('/api', userRoutes);
 app.use('/api', music2playlistRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-// Определяем порт
+
 const PORT = process.env.PORT || 5000;
 
-// Запускаем сервер
+
 app.listen(PORT, () => {
   console.log(`Сервер работает на порту http://localhost:${PORT}/api-docs`);
 });
